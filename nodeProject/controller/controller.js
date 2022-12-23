6b9f16021e1adaedd3f5c90d70143c5f75444d6b
unknown <yemova@gmail.com>
2022-12-23 17:33:59 -0500

    add try/catch for avoid crash server

--- nodeProject/controller/controller.js
+++ nodeProject/controller/controller.js
@@ -1,57 +1,67 @@
 const axios = require('axios')
 
 module.exports.getCommits = async (req, res) => {
-    const { gitHubUser, repoName, branchName } = req.params
+    try {
+        const { gitHubUser, repoName, branchName } = req.params
 
-    var config = {
-        method: 'GET',
-        url: `${process.env.URLGITHUBAPI}/${gitHubUser}/${repoName}/commits?sha=${branchName}`
-    };
+        var config = {
+            method: 'GET',
+            url: `${process.env.URLGITHUBAPI}/${gitHubUser}/${repoName}/commits?sha=${branchName}`
+        };
 
-    axios(config)
-        .then(function (response) {
-            let responseCommits = []
-            response.data.forEach(element => {
-                responseCommits.push({
-                    message: element.commit.message,
-                    date: element.commit.committer.date,
-                    login: element.committer.login,
-                    avatar_url: element.committer.avatar_url
+        axios(config)
+            .then(function (response) {
+                let responseCommits = []
+                response.data.forEach(element => {
+                    responseCommits.push({
+                        message: element.commit.message,
+                        date: element.commit.committer.date,
+                        login: element.committer.login,
+                        avatar_url: element.committer.avatar_url
+                    })
                 })
+                responseCommits.sort(function (a, b) {
+                    let keyA = a.date
+                    let keyB = b.date;
+                    if (keyA < keyB) return -1;
+                    if (keyA > keyB) return 1;
+                    return 0;
+                });
+                res.status(response.status).send(responseCommits)
             })
-            responseCommits.sort(function (a, b) {
-                let keyA = a.date
-                let keyB = b.date;
-                if (keyA < keyB) return -1;
-                if (keyA > keyB) return 1;
-                return 0;
+            .catch(function (error) {
+                res.status(error.response.status).send(error.message)
             });
-            res.status(response.status).send(responseCommits)
-        })
-        .catch(function (error) {
-            res.status(error.response.status).send(error.message)
-        });
+
+    } catch (error) {
+        res.status(500).send('Internal server error')
+    }
 };
 
 module.exports.getBranches = async (req, res) => {
-    const { gitHubUser, repoName } = req.params
+    try {
+        const { gitHubUser, repoName } = req.params
 
-    var config = {
-        method: 'GET',
-        url: `${process.env.URLGITHUBAPI}/${gitHubUser}/${repoName}/branches`
-    };
+        var config = {
+            method: 'GET',
+            url: `${process.env.URLGITHUBAPI}/${gitHubUser}/${repoName}/branches`
+        };
 
-    axios(config)
-        .then(function (response) {
-            let responseBranches = []
-            response.data.forEach(element => {
-                responseBranches.push({
-                    name: element.name
-                })
+        axios(config)
+            .then(function (response) {
+                let responseBranches = []
+                response.data.forEach(element => {
+                    responseBranches.push({
+                        name: element.name
+                    })
+                });
+                res.status(response.status).send(responseBranches)
+            })
+            .catch(function (error) {
+                res.status(error.response.status).send(error.message)
             });
-            res.status(response.status).send(responseBranches)
-        })
-        .catch(function (error) {
-            res.status(error.response.status).send(error.message)
-        });
+
+    } catch (error) {
+        res.status(500).send('Internal server error')
+    }
 };
