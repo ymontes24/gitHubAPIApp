7b9520c3ffac26e243491eaec3deb777dc5620a9
unknown <yemova@gmail.com>
2022-12-23 11:56:42 -0500

    add API request to the backend and sow commits info

--- angularPrject/commits-app/src/app/commit-list/components/commit-list/commit-list.component.ts
+++ angularPrject/commits-app/src/app/commit-list/components/commit-list/commit-list.component.ts
@@ -10,27 +10,52 @@ export class CommitListComponent implements OnInit {
   repoName: string = 'gitHubAPIApp';
   isVisible = false;
   isVisibleError = false;
-  branches:any = []
-  branchSelected: string | undefined
-
+  branches: any = [];
+  branchSelected: string ='';
+  commits: any = []
 
   constructor(private _commitsAppService: CommitsAppService) {}
 
   ngOnInit(): void {}
 
   onSubmit(form: any) {
-    this.branches = []
+    this.branches = [];
     this._commitsAppService
       .getBranches(form.value.userName, form.value.repoName)
-      .subscribe((res) => {
-        res.forEach((element: any) => {
-          this.branches.push(element.name)
-        });
-        this.isVisibleError = false;
-        this.isVisible = true;
-      },(err) => {
-        this.isVisibleError = true;
-        this.isVisible = false;
-      });
+      .subscribe(
+        (res) => {
+          res.forEach((element: any) => {
+            this.branches.push(element.name);
+          });
+          this.userName = form.value.userName;
+          this.repoName = form.value.repoName;
+          this.isVisibleError = false;
+          this.isVisible = true;
+        },
+        (err) => {
+          this.isVisibleError = true;
+          this.isVisible = false;
+        }
+      );
+  }
+
+  onSubmitCommits(form: any) {
+    console.log('====================================');
+    console.log(this.branchSelected);
+    console.log('====================================');
+    this._commitsAppService
+      .getCommitList(this.userName, this.repoName, this.branchSelected)
+      .subscribe(
+        (res) => {
+          console.log('====================================');
+          this.commits = res
+          console.log(res);
+          console.log('====================================');
+        },
+        (err) => {
+          this.isVisibleError = true;
+          this.isVisible = false;
+        }
+      );
   }
 }
